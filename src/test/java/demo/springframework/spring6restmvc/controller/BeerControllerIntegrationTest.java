5fa5b7db74b4fefd25a9970c3f49690393fae71c
Taejun Moon <junall10022@gmail.com>
2023-04-08 19:42:09 +0900

    test: Add beer controller integration test

---
+++ src/test/java/demo/springframework/spring6restmvc/controller/BeerControllerIntegrationTest.java
@@ -0,0 +1,130 @@
+package demo.springframework.spring6restmvc.controller;
+
+import demo.springframework.spring6restmvc.entities.Beer;
+import demo.springframework.spring6restmvc.mappers.BeerMapper;
+import demo.springframework.spring6restmvc.model.BeerDTO;
+import demo.springframework.spring6restmvc.repositories.BeerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+class BeerControllerIntegrationTest {
+
+    @Autowired
+    BeerController beerController;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Autowired
+    BeerMapper beerMapper;
+
+    @Test
+    void deleteByIdNotFound() {
+        assertThrows(NotFoundException.class, () -> {
+            beerController.deleteById(UUID.randomUUID());
+        });
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void deleteById() {
+        Beer beer = beerRepository.findAll().get(0);
+
+        ResponseEntity responseEntity = beerController.deleteById(beer.getId());
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(204));
+
+        assertThat(beerRepository.findById(beer.getId())).isEmpty();
+
+    }
+
+    @Test
+    void updateBeerNotFound() {
+        assertThrows(NotFoundException.class, ()->{
+            beerController.updateById(UUID.randomUUID(), BeerDTO.builder().build());
+        });
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void updateExistingBeer() {
+        Beer beer = beerRepository.findAll().get(0);
+        BeerDTO beerDTO = beerMapper.beerToBeerDto(beer);
+        beerDTO.setId(null);
+        beerDTO.setVersion(null);
+        final String beerName = "UPDATED";
+        beerDTO.setBeerName(beerName);
+
+        ResponseEntity responseEntity = beerController.updateById(beer.getId(), beerDTO);
+        Beer updatedBeer = beerRepository.findById(beer.getId()).get();
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(204));
+        assertThat(updatedBeer.getBeerName()).isEqualTo(beerName);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void saveNewBeer() {
+        BeerDTO beerDTO = BeerDTO.builder()
+                .beerName("New Beer")
+                .build();
+
+        ResponseEntity responseEntity = beerController.handlePost(beerDTO);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(201));
+        assertThat(responseEntity.getHeaders().getLocation()).isNotNull();
+
+        String[] locationUUID = responseEntity.getHeaders().getLocation().getPath().split("/");
+        UUID savedUUID = UUID.fromString(locationUUID[4]);
+
+        Beer beer = beerRepository.findById(savedUUID).get();
+        assertThat(beer).isNotNull();
+    }
+
+    @Test
+    void beerIdNotFound() {
+        assertThrows(NotFoundException.class, ()->{
+            beerController.getBeerById(UUID.randomUUID());
+        });
+    }
+
+    @Test
+    void getById() {
+        Beer beer = beerRepository.findAll().get(0);
+
+        BeerDTO dto = beerController.getBeerById(beer.getId());
+
+        assertThat(dto).isNotNull();
+    }
+
+    @Test
+    void listBeers() {
+        List<BeerDTO> dtoList = beerController.listBeers();
+
+        assertThat(dtoList.size()).isEqualTo(3);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void emptyListBeers() {
+        beerRepository.deleteAll();
+        List<BeerDTO> dtoList = beerController.listBeers();
+
+        assertThat(dtoList.size()).isEqualTo(0);
+    }
+}
