9762980f47473c4d7e66241d544efded33820118
Taejun Moon <junall10022@gmail.com>
2023-04-13 23:52:18 +0900

    fix: Fix table creation sql for beer and customer

--- src/main/java/demo/springframework/spring6restmvc/entities/Beer.java
+++ src/main/java/demo/springframework/spring6restmvc/entities/Beer.java
@@ -4,8 +4,11 @@ import demo.springframework.spring6restmvc.model.BeerStyle;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 
 import java.math.BigDecimal;
@@ -23,7 +26,8 @@ public class Beer {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
 
     @Version
@@ -31,6 +35,8 @@ public class Beer {
 
     @NotNull
     @NotBlank
+    @Size(max = 50)     // for element
+    @Column(length = 50)    // for persistent data
     private String beerName;
 
     @NotNull
