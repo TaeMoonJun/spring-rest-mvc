96946a79d6c051bd91c81e1dd97ff58c16462cc7
Taejun Moon <junall10022@gmail.com>
2023-04-15 05:41:12 +0900

    feat: Add beer csv, POJO and service

---
+++ src/main/java/demo/springframework/spring6restmvc/services/BeerCsvService.java
@@ -0,0 +1,10 @@
+package demo.springframework.spring6restmvc.services;
+
+import demo.springframework.spring6restmvc.model.BeerCSVRecord;
+
+import java.io.File;
+import java.util.List;
+
+public interface BeerCsvService {
+    List<BeerCSVRecord> convertCSV(File csvFile);
+}
