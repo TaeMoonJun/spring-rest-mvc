19c90712a5248ab5432f30ce49c54736aa8a0d07
Taejun Moon <junall10022@gmail.com>
2023-05-15 17:10:25 +0900

    test: Add test for BeerOrderRepository

---
+++ src/test/java/demo/springframework/spring6restmvc/repositories/BeerOrderRepositoryTest.java
@@ -0,0 +1,52 @@
+package demo.springframework.spring6restmvc.repositories;
+
+import demo.springframework.spring6restmvc.bootstrap.BootstrapData;
+import demo.springframework.spring6restmvc.entities.Beer;
+import demo.springframework.spring6restmvc.entities.BeerOrder;
+import demo.springframework.spring6restmvc.entities.Customer;
+import demo.springframework.spring6restmvc.services.BeerCsvServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@Import({BootstrapData.class, BeerCsvServiceImpl.class})
+class BeerOrderRepositoryTest {
+
+    @Autowired
+    BeerOrderRepository beerOrderRepository;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    Customer testCustomer;
+    Beer testBeer;
+
+    @BeforeEach
+    void setUp() {
+        testCustomer = customerRepository.findAll().get(0);
+        testBeer = beerRepository.findAll().get(0);
+    }
+
+    @Transactional
+    @Test
+    void beerOrders() {
+        BeerOrder beerOrder = BeerOrder.builder()
+                .customerRef("Test order")
+                .customer(testCustomer)
+                .build();
+
+        BeerOrder savedBeerOrder = beerOrderRepository.save(beerOrder);
+
+        assertThat(savedBeerOrder).isEqualTo(testCustomer.getBeerOrders().toArray()[0]);
+    }
+}
