10cf660903b7eccfb4686a1a35b65ae42d99489f
Taejun Moon <junall10022@gmail.com>
2023-05-15 17:12:36 +0900

    fix: Fix mapper reporting policy

--- src/main/java/demo/springframework/spring6restmvc/mappers/BeerMapper.java
+++ src/main/java/demo/springframework/spring6restmvc/mappers/BeerMapper.java
@@ -3,8 +3,9 @@ package demo.springframework.spring6restmvc.mappers;
 import demo.springframework.spring6restmvc.entities.Beer;
 import demo.springframework.spring6restmvc.model.BeerDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface BeerMapper {
     Beer beerDtoToBeer(BeerDTO dto);
 
--- src/main/java/demo/springframework/spring6restmvc/mappers/CustomerMapper.java
+++ src/main/java/demo/springframework/spring6restmvc/mappers/CustomerMapper.java
@@ -3,8 +3,9 @@ package demo.springframework.spring6restmvc.mappers;
 import demo.springframework.spring6restmvc.entities.Customer;
 import demo.springframework.spring6restmvc.model.CustomerDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CustomerMapper {
     Customer customerDtoTOCustomer(CustomerDTO dto);
 
