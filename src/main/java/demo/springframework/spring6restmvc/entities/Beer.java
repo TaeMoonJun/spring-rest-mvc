1eac76f8a13476ac113af9b40825092f588d41e9
Taejun Moon <junall10022@gmail.com>
2023-05-15 18:48:59 +0900

    feat: Add association helper method for Beer and Category

--- src/main/java/demo/springframework/spring6restmvc/entities/Beer.java
+++ src/main/java/demo/springframework/spring6restmvc/entities/Beer.java
@@ -70,4 +70,14 @@ public class Beer {
             joinColumns = @JoinColumn(name = "beer_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    public void addCategory(Category category) {
+        this.categories.add(category);
+        category.getBeers().add(this);
+    }
+
+    public void removeCategory(Category category) {
+        this.categories.remove(category);
+        category.getBeers().remove(this);
+    }
 }
